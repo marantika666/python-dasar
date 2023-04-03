a7c6fbdc3c4cbfd368e8846571c6b3d8cfc54cb7
Marantika Rhama Putra <98590984+Marantika666@users.noreply.github.com>
2023-04-04 01:39:11 +0700

    Add files via upload

---
+++ 1_tipe_data.py
@@ -0,0 +1,18 @@
+# tipe data string (kumpulan karakter)
+from tkinter import N
+
+
+data_string = "Marantika666"
+print("data :", data_string, ", merupakan data yang bertipe :", type(data_string))
+
+# tipe data integer (kumpulan angka)
+data_integer = 27
+print("data :", data_integer, ", merupakan data yang bertipe :", type(data_integer))
+
+# tipe data float (angka dengan koma)
+data_float = 27.03
+print("data :", data_float, ", merupakan data yang bertipe :", type(data_float))
+
+# tipe data boolean (biner true/false)
+data_bool = False
+print("data :", data_bool, ", merupakan data yang bertipe :", type(data_bool))
